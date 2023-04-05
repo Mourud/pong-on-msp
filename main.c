fbd140254e03cb785d424b0a6afb806ddddb351c
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2023-04-04 17:19:18 -0700

    works with noise on draw

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #define MOSI BIT0 // Master-out Slave-in
 #define SCK BIT2  // Serial clock
 
-
 unsigned char game_screen[8][102] = {0};
+
+int exit = 0;
+
 struct ball
 {
     int x;
@@ -26,14 +28,6 @@ struct paddle
     int score;
 };
 
-int updateScore(struct paddle *paddle);
-int getScore(struct paddle *paddle);
-
-// TODO: How to display on LCD
-// TODO: Handling inputs
-
-int exit = 0;
-
 int updateScore(struct paddle *paddle)
 {
     paddle->score++;
@@ -43,27 +37,6 @@ int updateScore(struct paddle *paddle)
 int getScore(struct paddle *paddle)
 {
     return paddle->score;
-}
-
-// void movePaddle(struct paddle *paddle, double force)
-//{
-//     paddle->x_vel = force;
-//     paddle->y_vel = force;
-//     paddle->x += paddle->x_vel;
-//     paddle->y += paddle->y_vel;
-// }
-
-// void gameLoop(){
-//     while(!exit){
-//         //update ball
-//         //update paddle
-//         //update score
-//         //update display
-//     }
-// }
-
-int onCollide()
-{
 }
 
 /* Write data to slave device.  Since the LCD panel is
@@ -318,25 +291,30 @@ int collides(struct ball *ball, struct paddle *player, struct paddle *computer)
     }
     return 0;
 }
-
-void main(void)
+void set_ball_speed(struct ball *ball, int count)
 {
-    // Stop the watchdog timer so it doesn't reset our chip
-    WDTCTL = WDTPW + WDTHOLD;
+    if (count % 100 == 0 && ball->x_vel < 10 && ball->x_vel > -10)
+    {
+        if (ball->x_vel > 0)
+        {
+            ball->x_vel++;
+        }
+        else
+        {
+            ball->x_vel--;
+        }
+    }
+}
 
+void init_SPI()
+{
     // These are the pins we need to drive.
     P3DIR |= SCK + MOSI + CS + CD;
-
     // De-select the LCD panel and set the clock high.
     P3OUT |= CS + SCK;
-
-    // Pause so everything has time to start up properly.
-    __delay_cycles(5500);
-
-    // Initialize the LCD panel.
-    init_lcd();
-
-    //    ADC
+}
+void init_ADC()
+{
     ADC12CTL0 = ADC12SHT02 + ADC12ON; // Sampling time, ADC12 on
     ADC12CTL1 = ADC12SHP;             // sampling timer
     ADC12CTL0 |= ADC12ENC;            // ADC enable
@@ -344,186 +322,204 @@ void main(void)
     ADC12MCTL0 = ADC12INCH_0;         // selects which input results are
                                       // stored in memory ADC12MEM0. Input
                                       // one is selected on reset so this line is not needed
-
+}
+void init_PWM()
+{
     P1DIR |= BIT2; // Output on Pin 1.2
     P1SEL |= BIT2; // Pin 1.2 selected as PWM
-
     // microseconds
     TA0CCTL1 = OUTMOD_7; // TA0CCR1 reset/set-high voltage
     // below count, low voltage when past
-
     TA0CTL = TASSEL_2 + MC_1 + TAIE + ID_0;
     // Timer A control set to SMCLK, 1MHz
     // and count up mode MC_1
-
-    // Notes of Baba Black Sheep
-    int notes[] = {261, 261, 392};
-    int score[] = {261, 392, 250};
-
+}
+void start_animation()
+{
     write_ones();
     write_zeros();
     write_ones();
     write_zeros();
+}
+
+void move_player(struct paddle *player, int y)
+{
+    if (y <= 0)
+    {
+        player->y = 0;
+    }
+    else if (y >= 49)
+    {
+        player->y = 49;
+    }
+    else
+    {
+        player->y = y;
+    }
+}
+
+void move_computer_insane(struct paddle *computer, int y, int ball_vel)
+{
+    if (y + ball_vel - computer->height / 2 < 0)
+    {
+        computer->y = 0;
+    }
+    else if (y + ball_vel - computer->height / 2 > 49)
+    {
+        computer->y = 49;
+    }
+    else
+    {
+        computer->y = y + ball_vel - computer->height / 2;
+    }
+}
+void move_computer_basic(struct paddle *computer, int y)
+{
+    if (y - computer->height / 2 < 0)
+    {
+        computer->y = 0;
+    }
+    else if (y - computer->height / 2 > 49)
+    {
+        computer->y = 49;
+    }
+    else
+    {
+        computer->y = y;
+    }
+}
+void play_music(int sel)
+{
+    int music[2][3];
+    int ball_hit[] = {261, 261, 392};
+    int score[] = {300, 350, 300};
     int i = 0;
-
-    struct ball
+    for (i = 0; i < 3; i++)
     {
-        int x;
-        int y;
-        int size;
-        int x_vel;
-        int y_vel;
-    };
-
-    struct paddle
+        music[0][i] = score[i];
+        music[1][i] = ball_hit[i];
+    }
+    i = 0;
+    for (i = 0; i < 6; i++)
     {
-        int x;
-        int y;
-        int height;
-        int width;
-        int y_vel;
-        int score;
-    };
+        int period = 1000000 / music[sel][i];
+        TA0CCR0 = period;
+        TA0CCR1 = period / 2;
+        __delay_cycles(50000);
+    }
+    TA0CCR0 = 0;
+    TA0CCR1 = 0;
+}
+void move_ball(struct ball *ball)
+{
+    if (ball->y <= 0 || ball->y >= 60)
+    {
+        ball->y_vel = -ball->y_vel;
+    }
+
+    ball->x += ball->x_vel;
+    ball->y += ball->y_vel;
+}
+
+char get_adc_position()
+{
+    ADC12CTL0 |= ADC12SC; // Start sampling
+    while (ADC12CTL1 & ADC12BUSY)
+        ; // while bit ADC12BUSY in register ADC12CTL1 is high wait
+    char adc_position = ADC12MEM0 >> 6;
+    return adc_position;
+}
+
+void set_up_game(struct paddle *player, struct paddle *computer, struct ball *ball)
+{
+    srand(time(0));
+
+    // Generate a random number between 0 and 1
+    int random_num = rand() % 2;
+    random_num = (random_num == 0) ? 1 : -1;
+    player->x = 10;
+    player->y = 49;
+    player->width = 7;
+    player->height = 15;
+    player->score = 0;
+    player->y_vel = 0;
+
+    computer->x = 85;
+    computer->y = 49;
+    computer->width = 7;
+    computer->height = 15;
+    computer->score = 0;
+
+    ball->x = 47;
+    ball->y = 28;
+    ball->size = 4;
+    ball->x_vel = 1;
+    ball->y_vel = random_num;
+}
+
+void check_collision(struct ball *ball, struct paddle *player, struct paddle *computer)
+{
+    if (collides(ball, player, computer))
+    {
+        play_music(1);
+        if (ball->x_vel < 0)
+        {
+            ball->x = player->x + player->width + 1;
+        }
+        else
+        {
+            ball->x = computer->x - ball->size - 1;
+        }
+        ball->x_vel = -ball->x_vel;
+    }
+}
+
+void update_score(struct paddle *player, struct paddle *computer, struct ball *ball)
+{
+    if (ball->x <= 0)
+    {
+        computer->score++;
+        play_music(0);
+        set_up_game(player, computer, ball);
+    }
+    else if (ball->x >= 95)
+    {
+        player->score++;
+        play_music(0);
+        set_up_game(player, computer, ball);
+    }
+}
+void main(void)
+{
+    // Stop the watchdog timer so it doesn't reset our chip
+    WDTCTL = WDTPW + WDTHOLD;
+    init_SPI();
+    __delay_cycles(5500); // Pause so everything has time to start up properly.
+    init_lcd();
+    init_ADC();
+    init_PWM();
+    start_animation();
     struct paddle player;
-    player.x = 10;
-    player.y = 49;
-    player.width = 7;
-    player.height = 15;
-    player.score = 0;
-    player.y_vel = 0;
-
     struct paddle computer;
-    computer.x = 85;
-    computer.y = 49;
-    computer.width = 7;
-    computer.height = 15;
-    computer.score = 0;
-
     struct ball ball;
-    ball.x = 47;
-    ball.y = 28;
-    ball.size = 4;
-    ball.x_vel = 1;
-    ball.y_vel = 1;
-
-    // test();
-    struct paddle old_player;
-    struct paddle old_computer;
-    struct ball old_ball;
+    set_up_game(&player, &computer, &ball);
     int count = 0;
 
     while (1)
     {
+        // set_ball_speed(&ball, count);
         count++;
-        if (count % 100 == 0 && ball.x_vel < 10 && ball.x_vel > -10)
-        {
-            if (ball.x_vel > 0)
-            {
-                ball.x_vel++;
-            }
-            else
-            {
-                ball.x_vel--;
-            }
-        }
-
-        ADC12CTL0 |= ADC12SC; // Start sampling
-        while (ADC12CTL1 & ADC12BUSY)
-            ; // while bit ADC12BUSY in register ADC12CTL1 is high wait
-        char c = ADC12MEM0 >> 6;
-
+        set_ball_speed(&ball, count);
+        char adc_position = get_adc_position();
         clear_rectangle(player.x, player.y, player.width, player.height);
         clear_rectangle(computer.x, computer.y, computer.width, computer.height);
         clear_rectangle(ball.x, ball.y, ball.size, ball.size);
-        if (c <= 0)
-        {
-            player.y = 0;
-        }
-        else if (c >= 49)
-        {
-            player.y = 49;
-        }
-        else
-        {
-            player.y = c;
-        }
-        if (ball.y + ball.y_vel - computer.height / 2 < 0)
-        {
-            computer.y = 0;
-        }
-        else if (ball.y + ball.y_vel - computer.height / 2 > 49)
-        {
-            computer.y = 49;
-        }
-        else
-        {
-            computer.y = ball.y + ball.y_vel - computer.height / 2;
-        }
-        // implement ball movement with bounce
-        ball.x += ball.x_vel;
-        ball.y += ball.y_vel;
-        if (ball.y <= 0 || ball.y >= 60)
-        {
-            ball.y_vel = -ball.y_vel;
-        }
-        if (collides(&ball, &player, &computer))
-        {
-            for (i = 0; i < 6; i++)
-            {
-                int period = 1000000 / notes[i];
-                TA0CCR0 = period;
-                TA0CCR1 = period / 2;
-                __delay_cycles(50000);
-            }
-            TA0CCR0 = 0;
-            TA0CCR1 = 0;
-            if (ball.x_vel < 0)
-            {
-                ball.x = player.x + player.width + 1;
-            }
-            else
-            {
-                ball.x = computer.x - ball.size - 1;
-            }
-            ball.x_vel = -ball.x_vel;
-        }
-        if (ball.x <= 0)
-        {
-            
-            ball.x = 47;
-            ball.y = 28;
-            ball.x_vel = 1;
-            ball.y_vel = 1;
-            computer.score++;
-            for (i = 0; i < 6; i++)
-            {
-                int period = 1000000 / score[i];
-                TA0CCR0 = period;
-                TA0CCR1 = period / 2;
-                __delay_cycles(50000);
-            }
-            TA0CCR0 = 0;
-            TA0CCR1 = 0;
-        }
-        if (ball.x >= 100)
-        {
-            
-            ball.x = 47;
-            ball.y = 28;
-            ball.x_vel = 1;
-            ball.y_vel = 1;
-            player.score++;
-            for (i = 0; i < 6; i++)
-            {
-                int period = 1000000 / score[i];
-                TA0CCR0 = period;
-                TA0CCR1 = period / 2;
-                __delay_cycles(50000);
-            }
-            TA0CCR0 = 0;
-            TA0CCR1 = 0;
-        }
-
+        move_player(&player, adc_position);
+        // move_computer_basic(&computer, ball.y);
+        move_computer_insane(&computer, ball.y, ball.y_vel);
+        move_ball(&ball);
+        update_score(&player, &computer, &ball);
+        check_collision(&ball, &player, &computer);
         draw_rectangle(player.x, player.y, player.width, player.height);
         draw_rectangle(computer.x, computer.y, computer.width, computer.height);
         draw_rectangle(ball.x, ball.y, ball.size, ball.size);
